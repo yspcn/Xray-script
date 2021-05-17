700d0df1f613fac5a717d3e5dfdff6645206e07e
eysp <60641231+eysp@users.noreply.github.com>
2021-05-17 16:17:40 +0800

    Update Xray-TLS+Web-setup.sh

--- Xray-TLS+Web-setup.sh
+++ Xray-TLS+Web-setup.sh
@@ -2408,11 +2408,9 @@ init_web()
     rm -rf "${nginx_prefix}/html/${true_domain_list[$1]}"
     if [ ${pretend_list[$1]} -eq 4 ]; then
         mkdir "${nginx_prefix}/html/${true_domain_list[$1]}"
-        unzip -q -d "${nginx_prefix}/html/${true_domain_list[$1]}" "${nginx_prefix}/html/Website.zip"
-#        mv "${nginx_prefix}/html/${true_domain_list[$1]}" "${nginx_prefix}/html/${true_domain_list[$1]}"
-        mv "${nginx_prefix}/html/${true_domain_list[$1]}/wordpress" "${nginx_prefix}/html/${true_domain_list[$1]}"
+        unzip -q -d "${nginx_prefix}/html" "${nginx_prefix}/html/Website.zip"
+        mv "${nginx_prefix}/html/wordpress" "${nginx_prefix}/html/${true_domain_list[$1]}"
         chown -R www-data:www-data "${nginx_prefix}/html/${true_domain_list[$1]}"
-#        rm -rf "${nginx_prefix}/html/wordpress"
     else
         unzip -q -d "${nginx_prefix}/html" "${nginx_prefix}/html/Website.zip"
         mv "${nginx_prefix}/html/nextcloud" "${nginx_prefix}/html/${true_domain_list[$1]}"
