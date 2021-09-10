fc8da3a3f5583acf0c513f081face51a86a97c3b
eysp <60641231+eysp@users.noreply.github.com>
2021-09-10 22:03:34 +0800

    Update redis-server.sh

--- redis-server.sh
+++ redis-server.sh
@@ -49,14 +49,14 @@ case "\$1" in
         fi
         ;;
     stop)
-        if [ ! -f $PIDFILE ]
+        if [ ! -f \$PIDFILE ]
         then
                 echo "\$PIDFILE does not exist, process is not running"
         else
                 PID=\$(cat $PIDFILE)
                 echo "Stopping ..."
                 $REDIS_CLI -p $REDISPORT SHUTDOWN
-                while [ -x ${PIDFILE} ]
+                while [ -x \${PIDFILE} ]
                do
                     echo "Waiting for Redis to shutdown ..."
                     sleep 1
@@ -65,8 +65,8 @@ case "\$1" in
         fi
         ;;
    restart|force-reload)
-        ${0} stop
-        ${0} start
+        \${0} stop
+        \${0} start
         ;;
   *)
     echo "Usage: /etc/init.d/redis {start|stop|restart|force-reload}" >&2
