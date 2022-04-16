7135b1a1934cfb18c04f3ec901f48c81e89257a9
eysp <60641231+eysp@users.noreply.github.com>
2022-04-16 17:12:21 +0800

    Update Xray-TLS+Web-setup.sh

--- Xray-TLS+Web-setup.sh
+++ Xray-TLS+Web-setup.sh
@@ -36,7 +36,7 @@ cloudreve_prefix="/usr/local/cloudreve"
 cloudreve_service="/etc/systemd/system/cloudreve.service"
 cloudreve_is_installed=""
 
-nextcloud_url="https://download.nextcloud.com/server/releases/nextcloud-22.2.0.zip"
+nextcloud_url="https://download.nextcloud.com/server/releases/nextcloud-23.0.3.zip"
 
 xray_config="/usr/local/etc/xray/config.json"
 xray_is_installed=""
