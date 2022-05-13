f823c3dd4bc0d4b53494c4aaf9cf939be8a7ecf5
eysp <60641231+eysp@users.noreply.github.com>
2022-05-13 13:11:19 +0800

    Update Xray-TLS+Web-setup.sh

--- Xray-TLS+Web-setup.sh
+++ Xray-TLS+Web-setup.sh
@@ -2624,10 +2624,10 @@ cat >> $nginx_config<<EOF
     location / {
         proxy_set_header Host  \$http_host;
         proxy_set_header X-Forwarded-Proto https;
-        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Upgrade \$http_upgrade;
         proxy_set_header Connection "upgrade";
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header HTTP_X_FORWARDED_FOR $remote_addr;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header HTTP_X_FORWARDED_FOR \$remote_addr;
         proxy_pass ${pretend_list[$i]};
         proxy_set_header referer "${pretend_list[$i]}";
         error_page 502 https://${true_domain_list[$i]}:843\$request_uri;
